1ff6e3918f421f1b98e106b44aeffb620c3ac267
sjwsl <xts.sjwsl@gmail.com>
2021-05-06 15:08:29 +0800

    feat(bootstrap): vim-plug

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,12 +17,17 @@ function init() {
 	fi
 
 	brew update
-
 	brew install coreutils
 	brew install git
 	brew install zplug
 	brew install neovim
 	brew install tmux
+
+  # install vim-plug
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  vim -c 'PlugInstall' -c 'q' -c 'q'
 
 	sync
 }
